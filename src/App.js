418baa6fb7aa6c37c74dbdd1ddc1e2dffd72143b
olenkashkol <olenkashkol@gmail.com>
2022-10-10 12:43:12 +0300

    add empty master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import {Route, Routes} from "react-router-dom";
-
-import {MainLayout} from "./mainLayout/MainLayout";
-import {ToDosPage, ToDoDetailsPage, CommentsPage, AlbumsPage, CommentPageDetails,AlbumsPageDetails} from "./pages";
 
 
 function App() {
     return (
         <div>
-            <Routes>
-                <Route path={'/'} element={<MainLayout/>}>
-                    <Route path={'todos'} element={<ToDosPage/>}>
-                        <Route path={':id'} element={<ToDoDetailsPage/>}/>
-                    </Route>
-                    <Route path={'albums'} element={<AlbumsPage/>}>
-                        <Route path={':id'} element={<AlbumsPageDetails/>}/>
-                    </Route>
-                    <Route path={'comments'} element={<CommentsPage/>}>
-                        <Route path={':id'} element={<CommentPageDetails/>}/>
-                    </Route>
-                </Route>
-            </Routes>
+
 
         </div>
     );
